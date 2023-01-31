23a74bcfc04af71b79129348f905657ebafcbd76
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-01-31 17:10:35 -0600

    cambios header

--- src/Components/Header.Component.jsx
+++ src/Components/Header.Component.jsx
@@ -13,18 +13,21 @@ export default function HeaderComponent() {
     setKeyword(evt.target.value)
    }
   return (
-    <>
+    <div>
       <Link to="/"><img src="/PokedexLogo.png" alt="PkLogo" className="mx-auto hover:cursor-pointer" /></Link>
-      <form className="max-w-xs m-auto" onSubmit={handleSubmit}>
+      <form className="m-auto" onSubmit={handleSubmit}> 
+      <div className="flex flex-row items-center">       
        <input
        onChange={handleChange}
           id="inputB"
           type="text"
-          placeholder="Ingresa un Pokemon . . ."
-          className="mt-5 shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Ingresa el nombre de un Pokemon"
+          className="mt-5 shadow appearance-none border rounded py-5 w-full px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           value={keyword}
         />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-5 py-5 px-3 rounded">Search</button>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
